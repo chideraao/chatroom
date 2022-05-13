a841af72f1c3511a742d8f4358a6ca96f947a29a
Chidera Okeke <okekechidera97@gmail.com>
2022-05-13 19:28:41 +0100

    minor edits

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,14 +2,15 @@
 import firebase from "firebase";
 
 /**setting up and initialising firebase config */
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAeoPiWLHXXkUuVYSA5BG2Oh7oF64vQ3H0",
-  authDomain: "dhera-gram.firebaseapp.com",
-  projectId: "dhera-gram",
-  storageBucket: "dhera-gram.appspot.com",
-  messagingSenderId: "577218277226",
-  appId: "1:577218277226:web:6055a44e41f9aa7f73bf13",
-  measurementId: "G-XLJYK0HW1J",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
